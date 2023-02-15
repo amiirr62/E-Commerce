8c58b57118a85d78f860638f3e80b2c89edc74c1
Amir <amiirr62@yahoo.com>
2023-02-14 17:32:05 -0800

    Must be logIn for using /products

--- routes/products.js
+++ routes/products.js
@@ -4,6 +4,14 @@ const Product = require('../models/products')
 const productController = require('../controllers/productsController')
 const productsValidator = require('../validators/productsValidator')
 
+
+router.use((req,res,next)=>{
+    if(req.isAuthenticated()){
+        return next()
+    }
+    res.redirect('/auth/login')
+})
+
 router.get('/', productController.showProducts)
 
 router.post('/',productsValidator.products(),productController.addProduct)
